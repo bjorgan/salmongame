eada384d2e0add4d7869f7a9373315c668a4f1ff
Asgeir Bjørgan <asgeirbj@samfundet.no>
2015-01-09 19:12:22 +0100

    Added preliminary bear character.

--- include/character.h
+++ include/character.h
@@ -1,11 +1,47 @@
-enum Direction{MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT};
+#ifndef CHARACTER_H_DEFINED
+#define CHARACTER_H_DEFINED
 
-int MAX_HEALTH = 100;
-int MIN_HEALTH = 0;
+
+
+#include <SDL/SDL.h>
+#include <SDL/SDL_gfxPrimitives.h>
 
 class Character{
 	public:
-		virtual void move(Direction direction); //move one step in specified direction
+		Character(int x, int y);
+		virtual void drawSprite(SDL_Surface *screen) = 0; //draw character sprite on screen
+
+		int get_x(){return x;};
+		int get_y(){return y;};
+		void set_x(int x){this->x = x;};
+		void set_y(int y){this->y = y;};
+	private:
+		int x, y;	
+};
+
+class Bear : public Character{
+	public:
+		Bear(int x, int y);
+		virtual void drawSprite(SDL_Surface *screen);
+	private:	
+		int width;
+		int height;
+		
+		int eye_width;
+		int eye_height;
+		int curr_eye_height;
+		int eye_dist;
+
+		int eye_y_diff;
+		int eye_x_diff;
+};
+
+
+
+
+
+
+/*		virtual void move(Direction direction); //move one step in specified direction
 		virtual void smashAttack() = {}; //special attack
 		void getCoordinates(int *x, int *y); //get current coordinates
 		void increaseHealth(int inc); //increase healthbar
@@ -15,6 +51,10 @@ class Character{
 		int x, y; //current coordinates of character
 		int health; //health bar
 };
+enum Direction{MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT};
+
+int MAX_HEALTH = 100;
+int MIN_HEALTH = 0;
 
 
 class Bear : public Character{
@@ -30,4 +70,7 @@ class Laks : public Character{
 		virtual void move(Direction direction);
 		virtual void smashAttack();
 		virtual void die();
-};
+};*/
+
+
+#endif
--- src/character.cpp
+++ src/character.cpp
@@ -1,61 +1,94 @@
 #include "character.h"
+#include <sys/time.h>
 
-void Character::move(Direction direction){
-	if (!dead){
-		switch direction{
-			case MOVE_UP:
-				y += step;
-			break;
-			case MOVE_DOWN:
-				y -= step;
-			break;
-			case MOVE_LEFT:
-				x -= step;
-			break;
-			case MOVE_RIGHT:
-				x += step;
-			break;
-		}
-	}
-}
-void Character::getCoordinates(int *x, int *y){
-	*x = this->x;
-	*y = this->y;
-}
-void Character::increaseHealth(int inc){
-	health += inc;
-	if (health > MAX_HEALTH){
-		health = MAX_HEALTH;
-	}
-}
-void Character::decreaseHealth(int dec){
-	health -= dec;
-	if (health <= MIN_HEALTH){
-		health = MIN_HEALTH;
-		this->die();
-	}
-}
-void Character::die(){
-	dead = true;
+Character::Character(int x, int y) : x(x), y(y){
 }
 
-
-void Bear::move(Direction direction){
-	Character::move(direction);
-}
-void Bear::smashAttack(){
-}
-void Bear::die(){
-	Character::die();
-}
-
-
-void Laks::move(Direction direction){
-	Character::move(direction);
-}
-void Laks::smashAttack(){
+Bear::Bear(int x, int y) : Character(x, y){
+	width = 100;
+	height = 200;
 	
+	//properties of the bear's eyes
+	eye_width = 10;
+	eye_height = 15;
+	eye_dist = 10;
+
+	eye_y_diff = -60;
+	eye_x_diff = 0;
 }
-void Laks::die(){
-	Character::die();
+
+void Bear::drawSprite(SDL_Surface *screen){
+	int eye_y = get_y() + eye_y_diff;
+	int eye_x = get_x() + eye_x_diff;
+	int x = get_x();
+	int y = get_y();
+
+	//body
+	boxRGBA(screen, x - width/2, y - height/2, x + width/2, y + height/2, 255, 0, 0, 150);
+
+	//left eye
+	boxRGBA(screen, eye_x - eye_dist/2 - eye_width , eye_y, x - eye_dist/2, eye_y - eye_height, 255, 255, 0, 255);
+
+	//right eye
+	boxRGBA(screen, eye_x + eye_dist/2 , eye_y, x + eye_dist/2 + eye_width, eye_y - eye_height, 255, 255, 0, 255);
 }
+
+//void Character::move(Direction direction){
+//	if (!dead){
+//		switch direction{
+//			case MOVE_UP:
+//				y += step;
+//			break;
+//			case MOVE_DOWN:
+//				y -= step;
+//			break;
+//			case MOVE_LEFT:
+//				x -= step;
+//			break;
+//			case MOVE_RIGHT:
+//				x += step;
+//			break;
+//		}
+//	}
+//}
+//void Character::getCoordinates(int *x, int *y){
+//	*x = this->x;
+//	*y = this->y;
+//}
+//void Character::increaseHealth(int inc){
+//	health += inc;
+//	if (health > MAX_HEALTH){
+//		health = MAX_HEALTH;
+//	}
+//}
+//void Character::decreaseHealth(int dec){
+//	health -= dec;
+//	if (health <= MIN_HEALTH){
+//		health = MIN_HEALTH;
+//		this->die();
+//	}
+//}
+//void Character::die(){
+//	dead = true;
+//}
+//
+//
+//void Bear::move(Direction direction){
+//	Character::move(direction);
+//}
+//void Bear::smashAttack(){
+//}
+//void Bear::die(){
+//	Character::die();
+//}
+//
+//
+//void Laks::move(Direction direction){
+//	Character::move(direction);
+//}
+//void Laks::smashAttack(){
+//	
+//}
+//void Laks::die(){
+//	Character::die();
+//}
